e4b36edcb50838ee69ab8159ff97c2e50a19f93c
manemis <manemis@users.noreply.github.com>
2021-06-13 19:13:11 +0200

    design modified

--- todo.js
+++ todo.js
@@ -34,8 +34,8 @@ function paintToDo(text){//16
     delBtn.innerText = "❌";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
-    li.appendChild(delBtn);
     li.appendChild(span);
+    li.appendChild(delBtn);
     li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
